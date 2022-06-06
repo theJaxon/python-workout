e841f9d65abebfd381f8c802cca16da8e1dc7004
Jaxon <mohamedmostels@gmail.com>
2022-06-06 21:07:52 +0200

    Update e33b2_passwd_to_dict.py

--- ch07-comprehensions/e33b2_passwd_to_dict.py
+++ ch07-comprehensions/e33b2_passwd_to_dict.py
@@ -1,14 +1,14 @@
-#!/usr/bin/env python3
+# Use a dict comprehension to create a dict in which the keys are usernames and
+# the values are (integer) user IDs, based on a Unix-style /etc/passwd file. Hint:
+# in a typical /etc/passwd file, the usernames are the first field in a row (i.e.,
+# index 0), and the user IDs are the third field in a row (i.e., index 2). If you need
+# to download a sample /etc/passwd file, you can get it from http://mng.bz/
+# 2XXg. Note that this sample file contains comment lines, meaning that you’ll
+# need to remove them when creating your dict.
 
-"""Solution to chapter 7, exercise 33, beyond 2: file_info"""
+def names_ids(path):
+ return {   line.split(':')[0]: line.split(':')[2]
+            for line in open(path)
+        }
 
-# Yes, this is a duplicate of exercise 33, beyond 2!  Whoops...
-
-import glob
-import os
-
-
-def file_info(dirname):
-    return {one_filename: os.stat(one_filename).st_size
-            for one_filename in glob.glob(f'{dirname}/*')
-            if os.path.isfile(one_filename)}
+print(names_ids('/etc/passwd'))
